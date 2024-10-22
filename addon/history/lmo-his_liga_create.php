f02b4271833ece056b5b6512bd4198ac9de58b2d
Henshingly <dietmar_kersting@web.de>
2024-10-22 07:40:06 +0200

    update
    code style better readable

--- addon/history/lmo-his_liga_create.php
+++ addon/history/lmo-his_liga_create.php
@@ -45,68 +45,68 @@ $defdateformat = "";
 // Open league file
 include PATH_TO_LMO . "/lmo-openfile.php";
 
-$fp = fopen(PATH_TO_LMO . '/' . $diroutput . basename($file) . '-tab.csv', "wb"); // Table CSV file
+$fp = fopen(PATH_TO_LMO . '/' . $diroutput . basename($file) . '-tab.csv', "wb");      //Table CSV file
 if ($st > 0) {
-  $rounds = $wert;
-  $act = $st;
+    $rounds = $wert;
+    $act = $st;
 }
 else {
-  $act = $stx;
+    $act = $stx;
 }
 for ($i1 = 0; $i1 < $anzsp; $i1 ++) {
-  if (isset($goala[$act-1][$i1]) && $goala[$act-1][$i1] == "-1") $goala[$act-1][$i1] = "_";
-  if (isset($goalb[$act-1][$i1]) && $goalb[$act-1][$i1] == "-1") $goalb[$act-1][$i1] = "_";
+    if (isset($goala[$act-1][$i1]) && $goala[$act-1][$i1] == "-1") $goala[$act-1][$i1] = "_";
+    if (isset($goalb[$act-1][$i1]) && $goalb[$act-1][$i1] == "-1") $goalb[$act-1][$i1] = "_";
 }
 
 $endtab = $anzst;
 include(PATH_TO_LMO . "/lmo-calctable.php");
 for ($i1 = 0; $i1 < $anzsp; $i1 ++) {
-  if (isset($goala[$act-1][$i1]) && $goala[$act-1][$i1] == "_") $goala[$act-1][$i1] = "-1";
-  if (isset($goalb[$act-1][$i1]) && $goalb[$act-1][$i1] == "_") $goalb[$act-1][$i1] = "-1";
+    if (isset($goala[$act-1][$i1]) && $goala[$act-1][$i1] == "_") $goala[$act-1][$i1] = "-1";
+    if (isset($goalb[$act-1][$i1]) && $goalb[$act-1][$i1] == "_") $goalb[$act-1][$i1] = "-1";
 }
 $x = 0;
 $j = 1;
 foreach ($tab0 as $y) {
-  $x ++;
-  $tabledata = explode('|', chunk_split($y, 8, "|"));
+    $x ++;
+    $tabledata = explode('|', chunk_split($y, 8, "|"));
 
-  fwrite($fp, $teams[$tabledata[4] - 50000000] . '|');                               //TeamLongName    (long Teamname)
-  fwrite($fp, $teamk[$tabledata[4] - 50000000] . '|');                               //TeamAbbrevation (short Teamname)
-  fwrite($fp, applyFactor($punkte[$tabledata[4] - 50000000], $pointsfaktor) . '|');  //Points+
-  if ($minus == 2) {
-    fwrite($fp, applyFactor($negativ[$tabledata[4] - 50000000], $pointsfaktor));     //Points-
-  }
-  fwrite($fp, '|');
-  fwrite($fp, applyFactor($etore[$tabledata[4] - 50000000], $goalfaktor) . '|');     //Goals+
-  fwrite($fp, applyFactor($atore[$tabledata[4] - 50000000], $goalfaktor) . '|');     //Goals-
-  fwrite($fp, $spiele[$tabledata[4] - 50000000] . '|');                              //Games
-  fwrite($fp, $siege[$tabledata[4] - 50000000] . '|');                               //Win
-  fwrite($fp, $unent[$tabledata[4] - 50000000] . '|');                               //Draw
-  fwrite($fp, $nieder[$tabledata[4] - 50000000] . '|');                              //Loss
-  if (($tabledata[4] - 50000000) == $favteam) {                                      //Marking Fav Team
-    fwrite($fp, "F");
-  }
-  if (($x == 1) && ($champ != 0)) {
-    fwrite($fp, "M");
-    $j = 2;
-  }
-  if (($x >= $j) && ($x < $j+$anzcl) && ($anzcl > 0)) {
-    fwrite($fp, "C");
-  }
-  if (($x >= $j + $anzcl) && ($x < $j + $anzcl + $anzck) && ($anzck > 0)) {
-    fwrite($fp, "Q");
-  }
-  if (($x >= $j + $anzcl + $anzck) && ($x < $j + $anzcl + $anzck + $anzuc) && ($anzuc > 0)) {
-    fwrite($fp, "U");
-  }
-  if (($x <= $anzteams - $anzab) && ($x > $anzteams - $anzab - $anzar) && ($anzar > 0)) {
-    fwrite($fp, "R");
-  }
-  if (($x <= $anzteams) && ($x > $anzteams - $anzab) && ($anzab > 0)) {
-    fwrite($fp, "A");
-  }
-  fwrite($fp, '|' . $teamm[$tabledata[4] - 50000000]);                               //TeamShortName (Medium-length Name)
-  fwrite($fp, "\n");
+    fwrite($fp, $teams[$tabledata[4] - 50000000] . '|');                               //TeamLongName (long Teamname)
+    fwrite($fp, $teamk[$tabledata[4] - 50000000] . '|');                               //TeamAbbrevation (short Teamname)
+    fwrite($fp, applyFactor($punkte[$tabledata[4] - 50000000], $pointsfaktor) . '|');  //Points+
+    if ($minus == 2) {
+        fwrite($fp, applyFactor($negativ[$tabledata[4] - 50000000], $pointsfaktor));   //Points-
+    }
+    fwrite($fp, '|');
+    fwrite($fp, applyFactor($etore[$tabledata[4] - 50000000], $goalfaktor) . '|');     //Goals+
+    fwrite($fp, applyFactor($atore[$tabledata[4] - 50000000], $goalfaktor) . '|');     //Goals-
+    fwrite($fp, $spiele[$tabledata[4] - 50000000] . '|');                              //Games
+    fwrite($fp, $siege[$tabledata[4] - 50000000] . '|');                               //Win
+    fwrite($fp, $unent[$tabledata[4] - 50000000] . '|');                               //Draw
+    fwrite($fp, $nieder[$tabledata[4] - 50000000] . '|');                              //Loss
+    if (($tabledata[4] - 50000000) == $favteam) {                                      //Marking Fav Team
+        fwrite($fp, "F");
+    }
+    if (($x == 1) && ($champ != 0)) {
+        fwrite($fp, "M");
+        $j = 2;
+    }
+    if (($x >= $j) && ($x < $j+$anzcl) && ($anzcl > 0)) {
+        fwrite($fp, "C");
+    }
+    if (($x >= $j + $anzcl) && ($x < $j + $anzcl + $anzck) && ($anzck > 0)) {
+        fwrite($fp, "Q");
+    }
+    if (($x >= $j + $anzcl + $anzck) && ($x < $j + $anzcl + $anzck + $anzuc) && ($anzuc > 0)) {
+        fwrite($fp, "U");
+    }
+    if (($x <= $anzteams - $anzab) && ($x > $anzteams - $anzab - $anzar) && ($anzar > 0)) {
+        fwrite($fp, "R");
+    }
+    if (($x <= $anzteams) && ($x > $anzteams - $anzab) && ($anzab > 0)) {
+        fwrite($fp, "A");
+    }
+    fwrite($fp, '|' . $teamm[$tabledata[4] - 50000000]);                               //TeamShortName (Medium-length Name)
+    fwrite($fp, "\n");
 }
 fclose($fp);
 ?>
--- addon/history/lmo-history.php
+++ addon/history/lmo-history.php
@@ -118,21 +118,21 @@ $aktJahr = date("Y");
 $output_sprachauswahl = "";
 
 // By Get certain parameters (for IFRAME)
-$m_liga       = isset($_GET['his_liga'])     ?  urldecode($_GET['his_liga'])    : '';
-$m_ligen      = isset($_GET['his_ligen'])    ?  urldecode($_GET['his_ligen'])   : '';
-$m_template   = isset($_GET['his_template']) ?  urldecode($_GET['his_template']): "history";
-$m_sort       = !empty($_GET['his_sort'])    ?  urldecode($_GET['his_sort'])    : '0';
-$m_headline   = !empty($_GET['his_headline'])?  urldecode($_GET['his_headline']): $text['history'][1];
-$archivFolder = isset($_GET['his_folder'])   ?  urldecode($_GET['his_folder'])  : (isset($his_folder) ?  $his_folder : basename($ArchivDir));  // Default
+$m_liga       = isset($_GET['his_liga'])     ?    urldecode($_GET['his_liga'])    : '';
+$m_ligen      = isset($_GET['his_ligen'])    ?    urldecode($_GET['his_ligen'])   : '';
+$m_template   = isset($_GET['his_template']) ?    urldecode($_GET['his_template']): "history";
+$m_sort       = !empty($_GET['his_sort'])    ?    urldecode($_GET['his_sort'])    : '0';
+$m_headline   = !empty($_GET['his_headline'])?    urldecode($_GET['his_headline']): $text['history'][1];
+$archivFolder = isset($_GET['his_folder'])   ?    urldecode($_GET['his_folder'])  : (isset($his_folder) ? $his_folder : basename($ArchivDir));  // Default
 
 // Directly certain parameters (for include/require)
-$m_liga       = isset($his_liga)          ?  $his_liga               : $m_liga;
-$m_ligen      = isset($his_ligen)         ?  $his_ligen              : $m_ligen;
-$m_template	  = isset($his_template)      ?  $his_template           : $m_template;
-$m_sort       = isset($his_sort)          ?  $his_sort               : $m_sort;
-$m_headline   = isset($his_headline)      ?  $his_headline           : $text['history'][1];
-$archivFolder = isset($_GET['his_folder'])?  $_GET['his_folder']     : (isset($his_folder) ?  $his_folder : basename($ArchivDir));  // Default
-$deflang      = isset($_POST["xdeflang"]) ?  trim($_POST["xdeflang"]): $deflang;
+$m_liga       = isset($his_liga)             ?    $his_liga                       : $m_liga;
+$m_ligen      = isset($his_ligen)            ?    $his_ligen                      : $m_ligen;
+$m_template	  = isset($his_template)         ?    $his_template                   : $m_template;
+$m_sort       = isset($his_sort)             ?    $his_sort                       : $m_sort;
+$m_headline   = isset($his_headline)         ?    $his_headline                   : $text['history'][1];
+$archivFolder = isset($_GET['his_folder'])   ?    $_GET['his_folder']             : (isset($his_folder) ? $his_folder : basename($ArchivDir));  // Default
+$deflang      = isset($_POST["xdeflang"])    ?    trim($_POST["xdeflang"])        : $deflang;
 
 if ($deflang == "") $deflang = "Deutsch";
 
@@ -141,7 +141,7 @@ include(PATH_TO_ADDONDIR . "/history/lmo-his_liga_create.php");
 
 // Language selection
 if ($einsprachwahl==1) {
-  $output_sprachauswahl = getLangSelector();
+    $output_sprachauswahl = getLangSelector();
 }
 
 // If IFRAME - complete HTML document
@@ -150,11 +150,11 @@ if (basename($_SERVER['PHP_SELF']) == "lmo-history.php") {?>
           "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-  <title>History</title>
-  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" >
-    <style type="text/css">
-      html,body {margin:0;padding:0;background:transparent;}
-    </style>
+    <title>History</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" >
+        <style type="text/css">
+            html,body {margin:0;padding:0;background:transparent;}
+        </style>
 </head>
 <body><?php
 }
@@ -164,133 +164,133 @@ if (basename($_SERVER['PHP_SELF']) == "lmo-history.php") {?>
   *  Teamname  |     Kurzname      | Pkt.+ | Pkt.- | Tore+| Tore-| Sp. | + | o  | -  |Markierung| Mittelname
 **/
 if ($cfgarray['history']['lmo_autocreate'] == 1) {
-  scan($archivFolder);
+    scan($archivFolder);
 }
 
 if (file_exists(PATH_TO_LMO . '/' . $diroutput . $m_liga . '-tab.csv')) {
-  $template = new HTML_Template_IT(PATH_TO_TEMPLATEDIR . '/history' );
-  $template -> loadTemplatefile($m_template . ".tpl.php");
+    $template = new HTML_Template_IT(PATH_TO_TEMPLATEDIR . '/history' );
+    $template -> loadTemplatefile($m_template . ".tpl.php");
 
-  $m_tabelle = array();
-  $handle = fopen (PATH_TO_LMO . '/' . $diroutput . $m_liga . '-tab.csv',"rb");
-  while (($data = fgetcsv ($handle, 1000, "|")) !== FALSE) {
-    $m_tabelle[$data[0]] = $data;
-    $m_tabelle[$data[0]][10] = "";
-    //Mannschaft in Liga
-    $m_tabelle[$data[0]][11] = 1;
-    //Meisterschaften
-    $m_tabelle[$data[0]][12] = 0;
-    //Abstiege
-    $m_tabelle[$data[0]][13] = 0;
-    //Saisons
-    $m_tabelle[$data[0]][14] = 1;
-  }
-  fclose($handle);
-  // Converting the passed leagues into an array
-  if ($m_ligen != '') {
-    $m_ligen = explode(', ', $m_ligen);
-    for ($i = 0; $i < count($m_ligen); $i ++) {
-      add_saison($m_tabelle,PATH_TO_LMO . '/' . $diroutput, $m_ligen[$i]);
+    $m_tabelle = array();
+    $handle = fopen (PATH_TO_LMO . '/' . $diroutput . $m_liga . '-tab.csv',"rb");
+    while (($data = fgetcsv ($handle, 1000, "|")) !== FALSE) {
+        $m_tabelle[$data[0]] = $data;
+        $m_tabelle[$data[0]][10] = "";
+        //Mannschaft in Liga
+        $m_tabelle[$data[0]][11] = 1;
+        //Meisterschaften
+        $m_tabelle[$data[0]][12] = 0;
+        //Abstiege
+        $m_tabelle[$data[0]][13] = 0;
+        //Saisons
+        $m_tabelle[$data[0]][14] = 1;
     }
-  }
-  else {
-    addLeague($archivFolder, $m_tabelle);
-  }
-  // Rekeying the table
-  $r_tabelle = array();
-  $keys = array_keys($m_tabelle);
-  for ($i = 0; $i < count($keys); $i ++) {
-    $r_tabelle[] = $m_tabelle[$keys[$i]];
-  }
-  $m_tabelle = $r_tabelle;
-  switch($m_sort) {
-    case(0):  usort($m_tabelle,'cmp0');   break; // Sorting by points (default)
-    case(1):  usort($m_tabelle,'cmp1');   break; // Sorted by victories
-    case(2):  usort($m_tabelle,'cmp2');   break; // Sorted by draw
-    case(3):  usort($m_tabelle,'cmp3');   break; // Sorted by defeats
-    case(4):  usort($m_tabelle,'cmp4');   break; // Sorting by games
-    case(5):  usort($m_tabelle,'cmp5');   break; // Sorting by +goals
-    case(6):  usort($m_tabelle,'cmp6');   break; // Sorting by diff. +goals/goals
-    case(7):  usort($m_tabelle,'cmp7');   break; // Sorting by average points per game
-    case(8):  usort($m_tabelle,'cmp8');   break; // Sorting by diff. +points/points
-    case(9):  usort($m_tabelle,'cmp9');   break; // Sorting by +points
-    case(10): usort($m_tabelle,'cmp10');  break; // Sorting by championships
-    case(11): usort($m_tabelle,'cmp11');  break; // Sorted by descent
-    case(12): usort($m_tabelle,'cmp12');  break; // Sorting by seasons
-  }
-  $m_anzteams = count($m_tabelle);
-  for ($j = 0; $j < $m_anzteams; $j ++) {
-    $template -> setCurrentBlock("Inhalt");
-    if (basename($_SERVER['PHP_SELF']) == "lmo-history.php") {
-      $sort_link = URL_TO_ADDONDIR . "/history/lmo-history.php?his_liga=$m_liga&amp;his_headline=$m_headline&amp;his_folder=$archivFolder&amp;his_template=$m_template&amp;";
+    fclose($handle);
+    // Converting the passed leagues into an array
+    if ($m_ligen != '') {
+        $m_ligen = explode(', ', $m_ligen);
+        for ($i = 0; $i < count($m_ligen); $i ++) {
+            add_saison($m_tabelle,PATH_TO_LMO . '/' . $diroutput, $m_ligen[$i]);
+        }
     }
     else {
-      $sort_link = $_SERVER['SCRIPT_NAME']."?";
+        addLeague($archivFolder, $m_tabelle);
     }
-    $template -> setVariable("LINK", $sort_link);
-    $template -> setVariable("Headline", $m_headline);
-    $template -> setVariable("Head_Teamname", $text[124]);
-    $template -> setVariable("copy", $text['history'][0]);
-    $template -> setVariable("Head_Championships", $text['history'][7]);
-    $template -> setVariable("Head_Descents", $text['history'][8]);
-    $template -> setVariable("Head_Playtimes", $text['history'][9]);
-    $template -> setVariable("Head_Matches", $text['history'][10]);
-    $template -> setVariable("Head_Won", $text['history'][11]);
-    $template -> setVariable("Head_Draw", $text['history'][12]);
-    $template -> setVariable("Head_Lost", $text['history'][13]);
-    $template -> setVariable("Head_Goals", $text['history'][14]);
-    $template -> setVariable("Head_Goalsdiff", $text['history'][15]);
-    $template -> setVariable("Head_Points", $text['history'][16]);
-    $template -> setVariable("Head_Pointsdiff", $text['history'][17]);
-    $template -> setVariable("Head_Points_Average", $text['history'][18]);
-    $template -> setVariable("TeamIcon", HTML_smallTeamIcon($m_liga, $m_tabelle[$j][0], " alt=''"));
-    $m_3punkte = ($m_tabelle[$j][7] * 3) + $m_tabelle[$j][8];
-    $template -> setVariable(array("Position"               => "<strong>" . ($j + 1) . "</strong>"));
-    $template -> setVariable(array("TeamLong"               => $m_tabelle[$j][0]));
-    $template -> setVariable(array("TeamMiddel"             => (isset($m_tabelle[$j][11])?$m_tabelle[$j][11]:'')));
-    $template -> setVariable(array("TeamShort"              => $m_tabelle[$j][1]));
-    $template -> setVariable(array("3Points"                => $m_3punkte));
-    $template -> setVariable(array("PlusPoints"             => $m_tabelle[$j][2]));
-    if ($m_tabelle[$j][3] != '') {
-      $template -> setVariable(array("MinusPoints"          => $m_tabelle[$j][3]));
-      if (($t_diff = $m_tabelle[$j][2] - $m_tabelle[$j][3]) > 0) $t_diff = '+' . $t_diff;
-      $template -> setVariable(array("Content_Pointsdiff"   => $t_diff));
+    // Rekeying the table
+    $r_tabelle = array();
+    $keys = array_keys($m_tabelle);
+    for ($i = 0; $i < count($keys); $i ++) {
+        $r_tabelle[] = $m_tabelle[$keys[$i]];
     }
-    $template -> setVariable(array("PlusGoals"              => $m_tabelle[$j][4]));
-    $template -> setVariable(array("MinusGoals"             => $m_tabelle[$j][5]));
-    if (($m_diff = $m_tabelle[$j][4] - $m_tabelle[$j][5]) > 0) $m_diff = '+' . $m_diff;
-    $template -> setVariable(array("Content_Goalsdiff"      => $m_diff));
-    $template -> setVariable(array("Content_Matches"        => $m_tabelle[$j][6]));
-    $template -> setVariable(array("Content_Won"            => $m_tabelle[$j][7]));
-    $template -> setVariable(array("Content_Draw"           => $m_tabelle[$j][8]));
-    $template -> setVariable(array("Content_Lost"           => $m_tabelle[$j][9]));
-    $style = '';
-    if ($m_tabelle[$j][6] > 0) {
-      $m_durch = round($m_tabelle[$j][2] / $m_tabelle[$j][6], 2);
+    $m_tabelle = $r_tabelle;
+    switch ($m_sort) {
+        case(0):  usort($m_tabelle,'cmp0');  break; // Sorting by points (default)
+        case(1):  usort($m_tabelle,'cmp1');  break; // Sorted by victories
+        case(2):  usort($m_tabelle,'cmp2');  break; // Sorted by draw
+        case(3):  usort($m_tabelle,'cmp3');  break; // Sorted by defeats
+        case(4):  usort($m_tabelle,'cmp4');  break; // Sorting by games
+        case(5):  usort($m_tabelle,'cmp5');  break; // Sorting by +goals
+        case(6):  usort($m_tabelle,'cmp6');  break; // Sorting by diff. +goals/goals
+        case(7):  usort($m_tabelle,'cmp7');  break; // Sorting by average points per game
+        case(8):  usort($m_tabelle,'cmp8');  break; // Sorting by diff. +points/points
+        case(9):  usort($m_tabelle,'cmp9');  break; // Sorting by +points
+        case(10): usort($m_tabelle,'cmp10'); break; // Sorting by championships
+        case(11): usort($m_tabelle,'cmp11'); break; // Sorted by descent
+        case(12): usort($m_tabelle,'cmp12'); break; // Sorting by seasons
     }
-    else {
-      $m_durch = 0;
+    $m_anzteams = count($m_tabelle);
+    for ($j = 0; $j < $m_anzteams; $j ++) {
+        $template -> setCurrentBlock("Inhalt");
+        if (basename($_SERVER['PHP_SELF']) == "lmo-history.php") {
+            $sort_link = URL_TO_ADDONDIR . "/history/lmo-history.php?his_liga=$m_liga&amp;his_headline=$m_headline&amp;his_folder=$archivFolder&amp;his_template=$m_template&amp;";
+        }
+        else {
+            $sort_link = $_SERVER['SCRIPT_NAME']."?";
+        }
+        $template -> setVariable("LINK", $sort_link);
+        $template -> setVariable("Headline", $m_headline);
+        $template -> setVariable("Head_Teamname", $text[124]);
+        $template -> setVariable("copy", $text['history'][0]);
+        $template -> setVariable("Head_Championships", $text['history'][7]);
+        $template -> setVariable("Head_Descents", $text['history'][8]);
+        $template -> setVariable("Head_Playtimes", $text['history'][9]);
+        $template -> setVariable("Head_Matches", $text['history'][10]);
+        $template -> setVariable("Head_Won", $text['history'][11]);
+        $template -> setVariable("Head_Draw", $text['history'][12]);
+        $template -> setVariable("Head_Lost", $text['history'][13]);
+        $template -> setVariable("Head_Goals", $text['history'][14]);
+        $template -> setVariable("Head_Goalsdiff", $text['history'][15]);
+        $template -> setVariable("Head_Points", $text['history'][16]);
+        $template -> setVariable("Head_Pointsdiff", $text['history'][17]);
+        $template -> setVariable("Head_Points_Average", $text['history'][18]);
+        $template -> setVariable("TeamIcon", HTML_smallTeamIcon($m_liga, $m_tabelle[$j][0], " alt=''"));
+        $m_3punkte = ($m_tabelle[$j][7] * 3) + $m_tabelle[$j][8];
+        $template -> setVariable(array("Position"               => "<strong>" . ($j + 1) . "</strong>"));
+        $template -> setVariable(array("TeamLong"               => $m_tabelle[$j][0]));
+        $template -> setVariable(array("TeamMiddel"             => (isset($m_tabelle[$j][11])?$m_tabelle[$j][11]:'')));
+        $template -> setVariable(array("TeamShort"              => $m_tabelle[$j][1]));
+        $template -> setVariable(array("3Points"                => $m_3punkte));
+        $template -> setVariable(array("PlusPoints"             => $m_tabelle[$j][2]));
+        if ($m_tabelle[$j][3] != '') {
+            $template -> setVariable(array("MinusPoints"        => $m_tabelle[$j][3]));
+            if (($t_diff = $m_tabelle[$j][2] - $m_tabelle[$j][3]) > 0) $t_diff = '+' . $t_diff;
+            $template -> setVariable(array("Content_Pointsdiff" => $t_diff));
+        }
+        $template -> setVariable(array("PlusGoals"              => $m_tabelle[$j][4]));
+        $template -> setVariable(array("MinusGoals"             => $m_tabelle[$j][5]));
+        if (($m_diff = $m_tabelle[$j][4] - $m_tabelle[$j][5]) > 0) $m_diff = '+' . $m_diff;
+        $template -> setVariable(array("Content_Goalsdiff"      => $m_diff));
+        $template -> setVariable(array("Content_Matches"        => $m_tabelle[$j][6]));
+        $template -> setVariable(array("Content_Won"            => $m_tabelle[$j][7]));
+        $template -> setVariable(array("Content_Draw"           => $m_tabelle[$j][8]));
+        $template -> setVariable(array("Content_Lost"           => $m_tabelle[$j][9]));
+        $style = '';
+        if ($m_tabelle[$j][6] > 0) {
+            $m_durch = round($m_tabelle[$j][2] / $m_tabelle[$j][6], 2);
+        }
+        else {
+            $m_durch = 0;
+        }
+        $template -> setVariable(array("Content_Points_Average" => $m_durch));
+        $template -> setVariable(array("Content_Championships"  => $m_tabelle[$j][12]));
+        $template -> setVariable(array("Content_Descents"       => $m_tabelle[$j][13]));
+        $template -> setVariable(array("Content_Playtimes"      => $m_tabelle[$j][14]));
+        $style = '';
+        if (strpos($m_tabelle[$j][10], 'F') !== FALSE)    //FavTeam {
+            $style .= "font-weight:bolder;";
+            $template -> setVariable(array("Style" => $style));
+        }
+        //Legende
+        $template -> setVariable(array("Meister"                => $text['history'][2]));
+        $template -> setVariable(array("Abstieg"                => $text['history'][3]));
+        $template -> setVariable(array("aktJahr"                => $aktJahr));
+        $template -> parseCurrentBlock();
     }
-    $template -> setVariable(array("Content_Points_Average" => $m_durch));
-    $template -> setVariable(array("Content_Championships"  => $m_tabelle[$j][12]));
-    $template -> setVariable(array("Content_Descents"       => $m_tabelle[$j][13]));
-    $template -> setVariable(array("Content_Playtimes"      => $m_tabelle[$j][14]));
-    $style = '';
-    if (strpos($m_tabelle[$j][10], 'F') !== FALSE)  //FavTeam {
-      $style .= "font-weight:bolder;";
-      $template -> setVariable(array("Style" => $style));
-    }
-    //Legende
-    $template -> setVariable(array("Meister"                => $text['history'][2]));
-    $template -> setVariable(array("Abstieg"                => $text['history'][3]));
-    $template -> setVariable(array("aktJahr"                => $aktJahr));
-    $template -> parseCurrentBlock();
-  }
-  $template -> setVariable("Language_Selection", $output_sprachauswahl);
-  $template -> show();
+    $template -> setVariable("Language_Selection", $output_sprachauswahl);
+    $template -> show();
 }
 else {
-  echo getMessage($text['history'][5] . " " . $his_liga, TRUE);
+    echo getMessage($text['history'][5] . " " . $his_liga, TRUE);
 }
 
 // If IFRAME - complete HTML document
