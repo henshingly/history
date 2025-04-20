9cbf03ce9b0181ca05b0d566f813b27705eb6649
Henshingly <dietmar_kersting@web.de>
2025-04-20 10:22:23 +0200

    Update lmo-history_func.php
    Update to match criteria of function fgetcsv() when using PHP 8.4.x
    
    Co-Authored-By: DwB <dummwiebrot@gmail.com>

--- addon/history/lmo-history_func.php
+++ addon/history/lmo-history_func.php
@@ -36,7 +36,7 @@
 function add_saison(&$m_tabelle, $path, $l_file) {
     $l_tabelle = array();
     $handle = fopen ($path . $l_file . '-tab.csv', "rb");
-    while (($data = fgetcsv ($handle, 1000, "|")) !== FALSE ) {
+    while (($data = fgetcsv($handle, 1000, '|', '\"', '\\')) !== FALSE) {
         $l_tabelle[] = $data;
     }
     fclose ($handle);
